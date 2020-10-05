ae414f2a80dda1a5bda2849f84ac1543bbf948d3
seandai318 <seandai318@gmail.com>
2020-10-04 19:14:58 -0500

    20201004, xml api change

--- common/src/diaConfig.c
+++ common/src/diaConfig.c
@@ -50,7 +50,7 @@ static uint32_t diaHostNum;
 
 void diaConfig_init(char* configFolder)
 {
-	osXmlDataCallbackInfo_t cbInfo={true, NULL, diaConfig_xmlData, DIA_XML_MAX_DATA_NAME_NUM};
+	osXmlDataCallbackInfo_t cbInfo={true, true, NULL, diaConfig_xmlData, DIA_XML_MAX_DATA_NAME_NUM};
     if(osXml_getLeafValue(configFolder, DIA_CONFIG_XSD_FILE_NAME, DIA_CONFIG_XML_FILE_NAME, &cbInfo) != OS_STATUS_OK)
     {
         logError("fails to diaConfig_getXmlConfig.");
